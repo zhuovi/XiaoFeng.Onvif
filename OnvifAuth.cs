ac9c015bccb06dd0a314137d3769263725cf1876
赵畅畅 <zhaochangchang@runaiot.com>
2022-10-13 18:26:05 +0800

    first commit

---
+++ OnvifAuth.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XiaoFeng.Onvif
+{
+    public class OnvifAuth
+    {
+        /// <summary>
+        /// 获取加密的字节数组
+        /// </summary>
+        /// <param name="nonce"></param>
+        /// <param name="createdString"></param>
+        /// <param name="basedPassword"></param>
+        /// <returns></returns>
+        private static byte[] buildBytes(string nonce, string createdString, string basedPassword)
+        {
+            byte[] nonceBytes = System.Convert.FromBase64String(nonce);
+            byte[] time = Encoding.UTF8.GetBytes(createdString);
+            byte[] pwd = Encoding.UTF8.GetBytes(basedPassword);
+
+            byte[] operand = new byte[nonceBytes.Length + time.Length + pwd.Length];
+            Array.Copy(nonceBytes, operand, nonceBytes.Length);
+            Array.Copy(time, 0, operand, nonceBytes.Length, time.Length);
+            Array.Copy(pwd, 0, operand, nonceBytes.Length + time.Length, pwd.Length);
+
+            return operand;
+        }
+
+        /// <summary>
+        /// 计算指定字节数组的哈希值。
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static byte[] SHAOneHash(byte[] data)
+        {
+            return HashAlgorithm.Create("SHA1").ComputeHash(data);
+        }
+
+        /// <summary>
+        /// 获取加密后的字符串
+        /// </summary>
+        /// <param name="nonce"></param>
+        /// <param name="createdString"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string GetPasswordDigest(string nonce, string createdString, string password)
+        {
+            byte[] combined = buildBytes(nonce, createdString, password);
+            string output = System.Convert.ToBase64String(SHAOneHash(combined));
+            return output;
+        }
+
+        /// <summary>
+        /// 加密的时间戳
+        /// </summary>
+        /// <param name="onvifUTCDateTime">从onvif获取的世间</param>
+        /// <returns></returns>
+        public static string GetCreated(DateTime onvifUTCDateTime)
+        {
+            return onvifUTCDateTime.ToString("yyyy-MM-ddThh:mm:ssZ");
+        }
+
+        /// <summary>
+        /// 将随机的16位字节数据加密成nonce
+        /// </summary>
+        /// <param name="nonce"></param>
+        /// <returns></returns>
+        public static string GetNonce()
+        {
+            byte[] nonce = new byte[16];
+            new Random().NextBytes(nonce);
+            return System.Convert.ToBase64String(nonce);
+        }
+
+        /// <summary>
+        /// 获取加密后的头部
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="onvifUTCDateTime"></param>
+        /// <returns></returns>
+        public static string GetHeadToken(string username, string password, DateTime onvifUTCDateTime)
+        {
+            string nonce = GetNonce();
+            string created = GetCreated(onvifUTCDateTime);
+            string pass = GetPasswordDigest(nonce, created, password);
+            //设置发送消息体
+            return $@"
+                              <s:Header xmlns:s=""http://www.w3.org/2003/05/soap-envelope"">
+                                 <wsse:Security xmlns:wsse=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"" 
+                                                xmlns:wsu=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd""> 
+                                           <wsse:UsernameToken> 
+                                               <wsse:Username>{username}</wsse:Username>
+                                               <wsse:Password Type=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest"">{pass}</wsse:Password>
+                                               <wsse:Nonce>{nonce}</wsse:Nonce> 
+                                               <wsu:Created>{created}</wsu:Created>
+                                           </wsse:UsernameToken>
+                                 </wsse:Security>
+                              </s:Header>";
+        }
+        /// <summary>
+        /// 信封头部
+        /// </summary>
+        /// <returns></returns>
+        public static string EnvelopeHeader()
+        {
+            return @"<s:Envelope xmlns:s=""http://www.w3.org/2003/05/soap-envelope"" xmlns:tds=""http://www.onvif.org/ver10/device/wsdl"" xmlns:tt=""http://www.onvif.org/ver10/schema"">";
+        }
+        /// <summary>
+        /// 信封内容
+        /// </summary>
+        /// <param name="bodyContent"></param>
+        /// <returns></returns>
+        public static string EnvelopeBody(string bodyContent)
+        {
+            return @$"  
+                              <s:Body>
+                                 {bodyContent}  
+                             </s:Body>";
+        }
+        /// <summary>
+        /// 信封尾部
+        /// </summary>
+        /// <returns></returns>
+        public static string EnvelopeFooter()
+        {
+            return @"
+                     </s:Envelope> "; 
+        }
+    }
+
+}
---
+++ OnvifDevice.cs
@@ -0,0 +1,221 @@
+﻿using System.Net;
+using XiaoFeng.Xml;
+
+namespace XiaoFeng.Onvif
+{
+    /// <summary>
+    /// 设备信息
+    /// </summary>
+    public class OnvifDevice
+    {
+        /// <summary>
+        /// 获取服务器时间
+        /// </summary>
+        public static async Task<DateTime> GetSystemDateAndTime(string ip)
+        {
+            var onvifUTCDateTime = DateTime.Now;
+            string reqMessageStr = "<tds:GetSystemDateAndTime/>";
+            var result = await Http.HttpHelper.GetHtmlAsync(new Http.HttpRequest
+            {
+                Method = HttpMethod.Post.ToString(),
+                ContentType = "application/xml",
+                Address = $"http://{ip}/onvif/device_service",
+                BodyData = $"{OnvifAuth.EnvelopeHeader()}{OnvifAuth.EnvelopeBody(reqMessageStr)}{OnvifAuth.EnvelopeFooter()}"
+            });
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var xnode_list = result.Html.XmlToEntity<XmlValue>();
+                    //不同设备 获取时间的节点也不一样
+                    if (   xnode_list.ChildNodes != null
+                        && xnode_list.ChildNodes[0].ChildNodes != null
+                        && xnode_list.ChildNodes[0].ChildNodes[0].ChildNodes != null
+                        && xnode_list.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes != null)
+                    {
+                        //UNIVIEW Camera
+                        var utcDateTime = xnode_list.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[3];
+                        var time_node = utcDateTime.ChildNodes[0];
+                        var date_node = utcDateTime.ChildNodes[1];
+
+                        var _hour = time_node[0].ToCast<int>();
+                        var _min = time_node[1].ToCast<int>();
+                        var _sec = time_node[2].ToCast<int>();
+
+                        var _year = date_node[0].ToCast<int>();
+                        var _month = date_node[1].ToCast<int>();
+                        var _day = date_node[2].ToCast<int>();
+
+                        onvifUTCDateTime = new DateTime(_year, _month, _day, _hour, _min, _sec);
+                    }
+                    if (xnode_list.ChildNodes != null
+                        && xnode_list.ChildNodes[1].ChildNodes != null
+                        && xnode_list.ChildNodes[1].ChildNodes[0].ChildNodes != null
+                        && xnode_list.ChildNodes[1].ChildNodes[0].ChildNodes[0].ChildNodes != null)
+                    {
+                        //Dahua Camera
+                        var utcDateTime = xnode_list.ChildNodes[1].ChildNodes[0].ChildNodes[0].ChildNodes[3];
+                        var time_node = utcDateTime.ChildNodes[0];
+                        var date_node = utcDateTime.ChildNodes[1];
+
+                        var _hour = time_node[0].ToCast<int>();
+                        var _min = time_node[1].ToCast<int>();
+                        var _sec = time_node[2].ToCast<int>();
+
+                        var _year = date_node[0].ToCast<int>();
+                        var _month = date_node[1].ToCast<int>();
+                        var _day = date_node[2].ToCast<int>();
+
+                        onvifUTCDateTime = new DateTime(_year, _month, _day, _hour, _min, _sec);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            else
+            {
+                /*请求失败*/
+            }
+            return onvifUTCDateTime;
+        }
+        /// <summary>
+        /// 初始化摄像头服务配置
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<string> GetCapabilities(string ip)
+        {
+            string reqMessageStr = @" 
+                                      <tds:GetCapabilities> 
+                                           <tds:Category>All</tds:Category> 
+                                      </tds:GetCapabilities> ";
+            var result = await Http.HttpHelper.GetHtmlAsync(new Http.HttpRequest
+            {
+                Method = HttpMethod.Post.ToString(),
+                ContentType = "application/xml",
+                Address = $"http://{ip}/onvif/device_service",
+                BodyData = $"{OnvifAuth.EnvelopeHeader()}{OnvifAuth.EnvelopeBody(reqMessageStr)}{OnvifAuth.EnvelopeFooter()}"
+            });
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var xnode_list = result.Html.XmlToEntity<XmlValue>();
+
+                    foreach (var item in xnode_list.Attributes)
+                    {
+                        var ll = item.Value;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+            else
+            {
+                /*请求失败*/
+            }
+            return "";
+        }
+        /// <summary>
+        /// 获取设备配置信息
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static async Task<string> GetProfiles(string ip, string user, string pass, DateTime onvifUTCDateTime)
+        {
+            var headToken = OnvifAuth.GetHeadToken(user, pass, onvifUTCDateTime);
+            string reqMessageStr = @"
+                                     <GetProfiles xmlns=""http://www.onvif.org/ver20/media/wsdl"">
+                                         <Type>All</Type>
+                                     </GetProfiles>";
+            var result = await Http.HttpHelper.GetHtmlAsync(new Http.HttpRequest
+            {
+                Method = HttpMethod.Post.ToString(),
+                ContentType = "application/xml",
+                Address = $"http://{ip}/onvif/media_service",
+                BodyData = $"{OnvifAuth.EnvelopeHeader()}{headToken}{OnvifAuth.EnvelopeBody(reqMessageStr)}{OnvifAuth.EnvelopeFooter()}"
+            });
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var value = result.Html;
+            }
+            else
+            {
+                /*请求失败*/
+            }
+            return "";
+        }
+        /// <summary>
+        /// 获取视频流地址
+        /// </summary>
+        /// <param name="token">Token</param>
+        /// <returns>视频流地址</returns>
+        public static async Task<string> GetStreamUri(string ip, string user, string pass, DateTime onvifUTCDateTime, string token)
+        {
+            var headToken = OnvifAuth.GetHeadToken(user, pass, onvifUTCDateTime);
+            //设置发送消息体
+            string reqMessageStr = $@"
+                                        <GetStreamUri xmlns=""http://www.onvif.org/ver10/media/wsdl"">
+                                          <StreamSetup>
+                                            <Stream xmlns=""http://www.onvif.org/ver10/schema"">RTP-Unicast</Stream>
+                                            <Transport xmlns=""http://www.onvif.org/ver10/schema"">
+                                              <Protocol>UDP</Protocol>
+                                            </Transport>
+                                          </StreamSetup>
+                                          <ProfileToken>{token}</ProfileToken>
+                                       </GetStreamUri>";
+            var result = await Http.HttpHelper.GetHtmlAsync(new Http.HttpRequest
+            {
+                Method = HttpMethod.Post.ToString(),
+                ContentType = "application/xml",
+                Address = $"http://{ip}/onvif/media_service",
+                BodyData = $"{OnvifAuth.EnvelopeHeader()}{headToken}{OnvifAuth.EnvelopeBody(reqMessageStr)}{OnvifAuth.EnvelopeFooter()}"
+            });
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var value = result.Html;
+            }
+            else
+            {
+                /*请求失败*/
+            }
+            return "";
+        }
+        /// <summary>
+        /// 获取视频快照地址
+        /// </summary>
+        /// <param name="token">Token</param>
+        /// <returns>视频快照地址</returns>
+        public static async Task<string> GetSnapshotUri(string ip, string user, string pass, DateTime onvifUTCDateTime, string token)
+        {
+            var headToken = OnvifAuth.GetHeadToken(user, pass, onvifUTCDateTime);
+            //设置发送消息体
+            string reqMessageStr = $@"
+                                        <GetSnapshotUri xmlns=""http://www.onvif.org/ver10/schema"">
+                                            <ProfileToken>{token}</trt:ProfileToken>
+                                        </GetSnapshotUri>";
+            var result = await Http.HttpHelper.GetHtmlAsync(new Http.HttpRequest
+            {
+                Method = HttpMethod.Post.ToString(),
+                ContentType = "application/xml",
+                Address = $"http://{ip}/onvif/media_service",
+                BodyData = $"{OnvifAuth.EnvelopeHeader()}{headToken}{OnvifAuth.EnvelopeBody(reqMessageStr)}{OnvifAuth.EnvelopeFooter()}"
+            });
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var value = result.Html;
+            }
+            else
+            {
+                /*请求失败*/
+            }
+            return "";
+        }
+    }
+
+}
