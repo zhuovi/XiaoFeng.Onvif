8d30c9e96666f83f4478a2be7a69c210e5f41515
赵畅畅 <zhaochangchang@runaiot.com>
2022-10-19 17:17:20 +0800

    命名空间

--- XmlNameSpace.cs
+++ XmlNameSpace.cs
@@ -9,7 +9,15 @@ namespace XiaoFeng.Onvif
     /// </summary>
     public class XmlNameSpace
     {
-        public string DeviceService = @" xmlns:tds=""http://www.onvif.org/ver10/device/wsdl"" xmlns:tt=""http://www.onvif.org/ver10/schema"" ";
-        public string EventService = @"xmlns:wsa=""http://www.w3.org/2005/08/addressing"" xmlns:tev=""http://www.onvif.org/ver10/events/wsdl""";
+        public static string DeviceService = @" xmlns:tds=""http://www.onvif.org/ver10/device/wsdl""
+                                                xmlns:tt=""http://www.onvif.org/ver10/schema"" ";
+        public static string EventService = @"xmlns:wsa=""http://www.w3.org/2005/08/addressing"" 
+                                              xmlns:tev=""http://www.onvif.org/ver10/events/wsdl""";
+        public static string MediaService = @"xmlns:trt=""http://www.onvif.org/ver10/media/wsdl""
+                                              xmlns:tt=""http://www.onvif.org/ver10/schema""";
+        public static string PTZService = @"xmlns:ter=""http://www.onvif.org/ver10/error""
+                                            xmlns:xs=""http://www.w3.org/2001/XMLSchema""
+                                            xmlns:tt=""http://www.onvif.org/ver10/schema""
+                                            xmlns:tptz=""http://www.onvif.org/ver20/ptz/wsdl""";
     }
 }
